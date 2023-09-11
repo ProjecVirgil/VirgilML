cbf37329a599e92cbf1d281bfaa673c932bc4919
Francesco è volevi <deadragon99@outlook.it>
2023-09-11 22:06:38 +0200

    Clean the code

--- script/delete_duplicate.py
+++ script/delete_duplicate.py
@@ -1,8 +1,5 @@
 # Carica il dataset di apprendimento italiano
 import pandas as pd
-
-
 dataset_learning_ita = pd.read_csv("appoggio.csv", delimiter=';').drop_duplicates()
 dataset_learning_ita.to_csv("appoggio.csv", sep=";", index=False, encoding='utf-8')
-
 print(len(dataset_learning_ita))
